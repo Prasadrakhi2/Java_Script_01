6a8b2401b1266d911e940d3ce05431ada33feeb7
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-05-14 13:39:00 +0000

    diffrent comparisions

---
+++ 01_Basics/04_conversion.js
@@ -0,0 +1,5 @@
+// ===  check the data type also
+// ==   only compair
+
+console.log("2" == 2)  //Output = true           only compair the value
+console.log("2" === 2)  //Output = false         compair the value + data type
