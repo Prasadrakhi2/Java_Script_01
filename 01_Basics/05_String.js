f0368e657d7c5b4f39fac53f028b77bf12c6281b
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-05-19 14:45:35 +0000

    string types

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,15 @@
+// const name = "rakhi"
+// const value = 32
+
+//concatination
+// console.log("my name is " + name + " and my value is " + value)
+
+//but we use String interpolation ${} and backtrick ` `
+// console.log(`hello my name is ${name} and my value is ${value}`) 
+
+const gameName = new String('rakhipr')  //it is like the array and give the indexing values also
+console.log(gameName[1])   //give the 1st index value = a
+console.log(gameName.__proto__)      //use 2 underscone and the function name for axis the function
+console.log(gameName.toUpperCase())
+console.log(gameName.length)
+console.log(gameName.indexOf('p'))
