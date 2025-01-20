2acbdf05a3ebcc326c7972461780be813d3e8954
rakhi <prasadrakhi20004@gmail.com>
2025-01-20 19:35:34 +0530

    secound code

---
+++ javaScript_html/_11event.js
@@ -0,0 +1,13 @@
+// events i.t occer on click
+// 1.click  = onclick()
+// 2.Double click = ondblclick();
+// 3.right click  = oncontextmenu();
+// 4.mouse hover  = onmouseenter()
+// 5.mouse down = onmousedown()
+// 6.mouse up = onmouseup()
+// 7.key press = onkeypress()   // for body
+// 8. key up = onkeyup()
+// 9. load = onload()     // for window
+// 10. onload = ununload()  
+// 11. resize =onresize()
+// 12 scroll = onscroll()
