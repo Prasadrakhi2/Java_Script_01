63c4e6443243bd5341e6628905aba821d9ef6839
rakhi <prasadrakhi20004@gmail.com>
2025-01-21 13:45:28 +0530

    third code

--- Basics/_06array.js
+++ Basics/_06array.js
@@ -1,4 +1,6 @@
-//an array is denoted by square brackets [ ]
+//1.an array is denoted by square brackets [ ]
+//2.js support multi data type in any array ex= [1, 3 , "rahi"];
+//3.any value is string then whole convert into the string
 
 var fruits = ["apple", "banana", "orange", "grape"];
 
